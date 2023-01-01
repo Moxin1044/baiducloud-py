76c1e394bf4764e387e56ce83824d11d3c622d45
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-01 08:59:43 +0800

    Fix the setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
+import io
+
+with io.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='baiducloud',
-    version='1.0.6',
+    version='1.0.8',
     packages=['baiducloud'],
     url='https://github.com/Moxin1044/baiducloud-py',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
     license='MIT License',
     author='Moxin',
     author_email='1044631097@qq.com',
